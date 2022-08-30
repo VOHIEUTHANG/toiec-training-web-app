ee931beafc866c6c5025a7f9ca7af3687989efc5
Vo Hieu Thang <77570864+VOHIEUTHANG@users.noreply.github.com>
2022-08-30 16:18:34 +0700

    config style for react project

---
+++ client/src/components/App/index.tsx
@@ -0,0 +1,12 @@
+import { DatePicker } from "antd";
+import "./style.css";
+
+function App() {
+  return (
+    <div className="app">
+      <DatePicker />
+    </div>
+  );
+}
+
+export default App;
