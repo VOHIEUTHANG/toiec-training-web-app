e1f3c52076a795b5ebbddf00d450b881188f2a66
Vo Hieu Thang <77570864+VOHIEUTHANG@users.noreply.github.com>
2022-09-02 23:41:06 +0700

    validate

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button as ButtonAntd } from "antd";
+import { useNavigate } from "react-router-dom";
 
 type PropsType = {
   children: string;
@@ -13,33 +14,46 @@ type PropsType = {
   rounded?: number;
   htmlType?: "submit";
   block?: boolean;
+  onClick?: () => void;
 };
 
 const Button = (props: PropsType) => {
+  const navigate = useNavigate();
   const { children, ...excludeChildrenProp } = props;
   const expectProps: { [key: string]: any } = {};
   const styles: { [key: string]: any } = {};
 
-  for (let key in excludeChildrenProp) {
-    const value = excludeChildrenProp[key as keyof typeof excludeChildrenProp];
-    switch (key) {
-      case "size":
-        expectProps.size =
-          value === "lg" ? "large" : value === "md" ? "middle" : "small";
-        break;
-      case "type":
-        expectProps.type = value === "outline" ? "ghost" : value;
-        break;
-      case "background":
-        styles.backgroundColor = value;
-        break;
-      case "rounded":
-        styles.boderRadius = `${value}px`;
-        break;
-      default:
-        expectProps[key] = value;
+  useEffect(() => {
+    for (let key in excludeChildrenProp) {
+      const value =
+        excludeChildrenProp[key as keyof typeof excludeChildrenProp];
+      switch (key) {
+        case "size":
+          expectProps.size =
+            value === "lg" ? "large" : value === "md" ? "middle" : "small";
+          break;
+        case "type":
+          expectProps.type = value === "outline" ? "ghost" : value;
+          break;
+        case "background":
+          styles.backgroundColor = value;
+          break;
+        case "rounded":
+          styles.boderRadius = `${value}px`;
+          break;
+        // case "href":
+        //   expectProps.onClick = () => {
+        //     console.log(value);
+        //     navigate(value as string);
+        //   };
+        //   break;
+        default:
+          expectProps[key] = value;
+      }
     }
-  }
+  });
+
+  console.log("expectProps ===> ", expectProps);
 
   return (
     <ButtonAntd className="my-btn" {...expectProps} style={styles}>
--- client/src/page/Register/index.tsx
+++ client/src/page/Register/index.tsx
@@ -1,10 +1,101 @@
-import React from "react";
+import { Form, Input } from "antd";
+import Button from "../../components/Button";
 
 const Register = () => {
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
   return (
     <div className="container">
-      <div className="flex flex-col justify-center items-center h-full">
-        <div>Register</div>
+      <div className="flex flex-col justify-center items-center page-wrapper h-full">
+        <div className="form-container">
+          <div className="form-title">Register</div>
+          <Form
+            name="basic"
+            labelCol={{ span: 7 }}
+            wrapperCol={{ span: 17 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+            size="large"
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Full name"
+              name="name"
+              rules={[
+                { required: true, message: "Please input your full name!" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                { required: true, message: "Please input your full name!" },
+                {
+                  pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                  message: "Invalid email type!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item
+              label="Configm password"
+              name="confirmPassword"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your configm password!",
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <div className="flex justify-between">
+              <Button type="link">Available acccount</Button>
+              <Button type="link">Return home</Button>
+            </div>
+
+            <Form.Item wrapperCol={{ offset: 10, span: 16 }}>
+              <Button type="outline" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
       </div>
     </div>
   );
