e3b32facf4c77e3eb3b1217d6f2de1a7e4cb0922
Vo Hieu Thang <77570864+VOHIEUTHANG@users.noreply.github.com>
2022-09-05 11:12:58 +0700

    feat(client): home page - testkit

---
+++ client/src/components/TestKitList/index.tsx
@@ -0,0 +1,25 @@
+import TestKit, { PropsType as TestKitType } from "../TestKit";
+import { Row, Col } from "antd";
+
+export type TestKitListType = TestKitType[];
+type PropsType = {
+  testKitList: TestKitListType;
+};
+
+const TestKitList = ({ testKitList }: PropsType) => {
+  return (
+    <div>
+      <Row gutter={[16, 16]}>
+        {testKitList.map((test, index) => {
+          return (
+            <Col key={index} xs={24} sm={24} md={12} lg={8} xl={8}>
+              <TestKit {...test} />
+            </Col>
+          );
+        })}
+      </Row>
+    </div>
+  );
+};
+
+export default TestKitList;
