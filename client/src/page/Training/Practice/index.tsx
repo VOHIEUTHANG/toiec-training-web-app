18e8828c1b54c361fd02fed654230eb4a2eaa195
Vo Hieu Thang <77570864+VOHIEUTHANG@users.noreply.github.com>
2022-09-06 22:06:56 +0700

    feat(client): traning page

---
+++ client/src/page/Training/Practice/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PageFrame from "../../../components/PageFrame";
+
+const PracticePage = () => {
+  return (
+    <PageFrame>
+      <div>Practice Page</div>
+    </PageFrame>
+  );
+};
+
+export default PracticePage;
