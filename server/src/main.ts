d54d006d32fb05d58b2bd7d0f6db4491a5771e61
Vo Hieu Thang <77570864+VOHIEUTHANG@users.noreply.github.com>
2022-09-17 23:41:12 +0700

    feat(add more data for part 1):

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,11 @@ import express from "express";
 import configs from "./configs";
 import initRoutes from "./routes";
 import connectMongoDB from "./database/connectDB";
+import QuestionModel from "./models/Question";
 require("./models/Question");
 require("./models/Package");
 require("./models/Part");
+
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -17,8 +19,30 @@ const PORT = process.env.PORT ?? 3000;
 
 connectMongoDB((error) => {
   if (!error) {
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
       console.log(`Server is listening on URL: http://localhost:${PORT}`);
+      // const newData = await QuestionModel.create({
+      //   audio:
+      //     "https://storage.googleapis.com/kstoeic/sound/eco2018_Test06_0101.mp3",
+      //   photo:
+      //     "https://storage.googleapis.com/kstoeic/images/7354108_1564544432903.png",
+      //   answerSheetList: [
+      //     {
+      //       question: "",
+      //       answerList: [
+      //         { key: "A", value: "She has some grocery bags", result: false },
+      //         { key: "B", value: "She is holding some flowers", result: false },
+      //         {
+      //           key: "C",
+      //           value: "She is reaching out to pick up a vegetable",
+      //           result: true,
+      //         },
+      //         { key: "D", value: "She is washing the fruits", result: false },
+      //       ],
+      //     },
+      //   ],
+      // });
+      // console.log(newData._id);
     });
   } else {
     console.log(error?.message);
